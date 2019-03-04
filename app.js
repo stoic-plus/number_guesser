4c15cda2331b47b867982be7c1a476a6b2f14106
stoic-plus <ricardoledesmadev@gmail.com>
2019-03-04 15:46:27 -0700

    changes app.js to return prototype object - init auto create a number to guess

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 module.exports = {
-  numToGuess: function(min, max){
+  generateGuessNum: function(min=1, max=11){
     return Math.floor(Math.random() * (max - min)) + min;
   },
-  evaluate: function(num, guess) {
-    if (num < guess) {
+  init: function(min=1, max=11) {
+    this.numToGuess = this.generateGuessNum(min, max);
+    console.log(this.numToGuess);
+    return this;
+  },
+  evaluate: function(guess) {
+    if (guess < this.numToGuess) {
       return "That is too low";
-    } else if (num > guess) {
+    } else if (guess > this.numToGuess) {
       return "That is too high";
     } else {
       return "BOOM!";

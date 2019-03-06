b0b5840a137311133708a0fca9acb6671d7f616f
stoic-plus <ricardoledesmadev@gmail.com>
2019-03-05 17:29:57 -0700

    adds functions to update dom to gameUI module

--- scripts/numberGuesserUI.js
+++ scripts/numberGuesserUI.js
@@ -5,6 +5,22 @@ define(function() {
         domElement.placeholder = 'You must enter a value';
         return true;
       }
+    },
+    tempDisplay: function(domElement, message) {
+      domElement.classList.remove('hidden');
+      domElement.innerText = message;
+      window.setTimeout(() => {
+        domElement.innerText = '';
+        domElement.classList.add('hidden');
+      }, 3000);
+    },
+    showFeedback: function(lastGuessDom, lastGuess, feedbackDom, feedback) {
+      lastGuessDom.innerText = lastGuess;
+      feedbackDom.innerText = feedback;
+    },
+    setRange: function(minDom, maxDom, min, max) {
+      minDom.innerText = min;
+      maxDom.innerText = max;
     }
   }
 })

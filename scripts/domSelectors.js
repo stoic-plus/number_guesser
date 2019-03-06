cd0173c81f1407c7370568faacb6521112eace65
stoic-plus <ricardoledesmadev@gmail.com>
2019-03-05 17:27:24 -0700

    adds selectors

--- scripts/domSelectors.js
+++ scripts/domSelectors.js
@@ -3,9 +3,17 @@ define(function() {
     guessInput: document.getElementById('guess-input'),
     makeGuess: document.getElementById('make-guess'),
     clearGuess: document.getElementById('clear-guess'),
-    feedback: document.querySelector('.feedback'),
+    feedback: document.getElementById('feedback'),
     lastGuess: document.getElementById('guess-last'),
     guessEval: document.getElementById('guess-evaluate'),
-    reset: document.getElementById('reset')
+    reset: document.getElementById('reset'),
+    rangeForm: document.getElementById('range-form'),
+    minRange: document.getElementById('min-range'),
+    maxRange: document.getElementById('max-range'),
+    submitRange: document.getElementById('range-submit'),
+    gameUI: document.getElementById('gameUI'),
+    minRangeDisplay: document.getElementById('min-range-display'),
+    maxRangeDisplay: document.getElementById('max-range-display'),
+    tempDisplay: document.getElementById('temp-display')
   }
 })
